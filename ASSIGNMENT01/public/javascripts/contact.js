6382cbcb816b53dc27245850983d375991498c3b
Daniel Pizarro Saavedra <Saavedra.Alejandro@outlook.com>
2025-02-17 19:35:02 -0500

    FInish Project
    1. Add Images and SVGs
         -  Include images where needed (logos, illustrations, icons, etc.).
         -  Optimize image formats (JPG, PNG, SVG) to improve performance.
         -  Ensure images are responsive and adapt well to different screen sizes.
         -  Add SVGs for better quality and scalability of icons or graphics.
    
    2. Add and Fix CSS
         -  Review and improve the visual design for better aesthetics and consistency.
         -  Fix styling issues (alignment, spacing, fonts, colors).
         -  Optimize CSS by removing unnecessary or redundant styles.
         -  Ensure the design is responsive across different devices.
    
    3. Create New Views
         -  Design and implement new pages or sections in the application.
         -  Define the structure and navigation between the new views.
         -  Integrate the necessary data and functionality into each view.
         -  Ensure consistency with the overall project design.

---
+++ ASSIGNMENT01/public/javascripts/contact.js
@@ -0,0 +1,3 @@
+const sendMeEmail = () => {
+    window.location.href = 'mailto:Saavedra.Alejandro@outlook.com?subject=Job opportunity – Contacto&body=Hello Daniel, I would like to speak with you about a job opportunity....';
+}
--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'home' });
 });
+
+router.get('/experience', function(req, res, next) {
+  res.render('experience', { title: 'experience' });
+});
+
+router.get('/programming', function(req, res, next) {
+  res.render('programming', { title: 'programming' });
+});
+
+
+router.get('/aboutMe', function(req, res, next) {
+  res.render('aboutMe', { title: 'aboutMe' });
+});
+
+router.get('/project', function(req, res, next) {
+  res.render('project', { title: 'project' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'contact'})
+})
 
 module.exports = router;
