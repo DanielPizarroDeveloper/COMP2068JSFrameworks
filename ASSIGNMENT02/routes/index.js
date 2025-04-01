2b3ad11d961e182084918bfd075601b711346f7a
Daniel Pizarro Saavedra <Saavedra.Alejandro@outlook.com>
2025-04-01 18:17:39 -0400

    Refactoring code
    Code refactoring, creating a more modular code

--- ASSIGNMENT02/routes/index.js
+++ ASSIGNMENT02/routes/index.js
@@ -1,103 +1,17 @@
 var express = require('express');
 var router = express.Router();
-const passport = require('passport');
-
-//USER CRUD
-const { registerUser } = require('../Controllers/User/user');
-
-//Message
-const { handleIncompleteSignup, handleFailedSignIn, handleSigninError } = require('../public/mocks/Message');
+const usersRouter = require('./users');
 
 const isAuthenticated = require('../middlewares/Auth');
 
-/* GET home page. */
 router.get('/', isAuthenticated, (req, res, next) => {
   res.render('index', { title: 'Home', user: req.user.username });
 });
 
-/* GET about page. */
 router.get('/about', isAuthenticated, (req, res, next) => {
   res.render('about', { title: 'About', user: req.user.username });
 });
 
-/* GET login page. */
-router.get('/login', function(req, res, next) {
-  //handle session message
-  let messages = req.session.messages || []; //extract messages, or set to empty if undefined
-  req.session.messages = []; // clear message
-  res.render('login', { title: 'Login', messages: messages });
-});
-
-/* POST login page. */
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/', // where to go if login is successful
-  failureRedirect: '/login', // where to go if login fails
-  failureMessage: handleSigninError() //additional message for login failure
-}));
-
-/* GET register page. */
-router.get('/register', function(req, res, next) {
-  const errorMessage = req.session.errorMessage;
-  req.session.errorMessage = null;
-  res.render('register', { title: 'Register', errorMessage });
-});
-
-/* POST register page. */
-router.post('/register', async(req, res, next) => {
-  try {
-    if(!req.body.username || !req.body.email || !req.body.password) {
-      throw new Error(handleIncompleteSignup());
-    } 
-    else {
-      const newUser = await registerUser(req.body.username, req.body.email, req.body.password);
-      req.login(newUser, (error) => {
-        if(error) {
-          throw new Error (handleFailedSignIn());
-        }
-        res.redirect('/');
-      });
-    }
-  }
-  catch(error) {
-    req.session.errorMessage = error.message;
-    res.redirect('/register');
-  }
-});
-
-/* GET logout */
-router.get('/logout', (req, res, next) => {
-  req.logout((error) => {
-    res.redirect('/login');
-  });
-});
-
-/* Get Account */
-router.get('/account', isAuthenticated, (req, res, next) => {
-  res.render('account', { title: 'Account', user: req.user.username, email: req.user.email });
-});
-
-/* Get GitHub */
-router.get('/github',
-  passport.authenticate('github', { scope: ['profile', 'email']})
-);
-
-router.get('/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login', }),
-  (req, res, next) => {
-    res.redirect('/')
-  }
-);
-
-/* Get Google */
-router.get('/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
-);
-
-router.get('/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login', }),
-  (req, res, next) => {
-    res.redirect('/')
-  }
-);
+router.use('/', usersRouter);
 
 module.exports = router;
--- ASSIGNMENT02/routes/users.js
+++ ASSIGNMENT02/routes/users.js
@@ -1,9 +1,82 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const isAuthenticated = require('../middlewares/Auth');
+
+const { registerUser } = require('../Controllers/User/user');
+const { handleIncompleteSignup, handleFailedSignIn, handleSigninError } = require('../public/mocks/Message');
+
+router.get('/login', function(req, res, next) {
+  //handle session message
+  let messages = req.session.messages || []; //extract messages, or set to empty if undefined
+  req.session.messages = []; // clear message
+  res.render('login', { title: 'Login', messages: messages });
 });
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/', // where to go if login is successful
+  failureRedirect: '/login', // where to go if login fails
+  failureMessage: handleSigninError() //additional message for login failure
+}));
+
+router.get('/register', function(req, res, next) {
+  const errorMessage = req.session.errorMessage;
+  req.session.errorMessage = null;
+  res.render('register', { title: 'Register', errorMessage });
+});
+
+router.post('/register', async(req, res, next) => {
+  try {
+    if(!req.body.username || !req.body.email || !req.body.password) {
+      throw new Error(handleIncompleteSignup());
+    } 
+    else {
+      const newUser = await registerUser(req.body.username, req.body.email, req.body.password);
+      req.login(newUser, (error) => {
+        if(error) {
+          throw new Error(handleFailedSignIn());
+        }
+        res.redirect('/');
+      });
+    }
+  }
+  catch(error) {
+    req.session.errorMessage = error.message;
+    res.redirect('/register');
+  }
+});
+
+router.get('/logout', (req, res, next) => {
+  req.logout((error) => {
+    res.redirect('/login');
+  });
+});
+
+router.get('/account', isAuthenticated, (req, res, next) => {
+  res.render('account', { title: 'Account', user: req.user.username, email: req.user.email });
+});
+
+router.get('/github',
+  passport.authenticate('github', { scope: ['profile', 'email']})
+);
+
+router.get('/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login', }),
+  (req, res, next) => {
+    res.redirect('/')
+  }
+);
+
+router.get('/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login', }),
+  (req, res, next) => {
+    res.redirect('/')
+  }
+);
 
 module.exports = router;
